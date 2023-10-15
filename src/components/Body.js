9d60de040bed5c57427f3bc6ddec14a3b4898e69
Amir H K <amirhkarimi888@gmail.com>
2023-10-15 20:52:02 +0330

    Some changes in grid styling of UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,76 +1,76 @@
 export const Body = `
 <main>
-    <div class="mx-auto w-full mt-[75px] flex lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 max-sm:grid-cols-1">
-        <div class="border border-gray-500 lg:w-1/4 md:w-full sm:w-full h-screen max-sm:w-full overflow-y-scroll">
+    <div class="mx-auto w-full mt-[75px] grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1">
+        <div class="h-screen max-sm:w-full overflow-y-scroll">
             <ul class="w-full mx-auto grid grid-cols-1">
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
                 <li class="grid grid-cols-2 cursor-pointer justify-center items-center p-3 border-b border-gray-200 hover:bg-gray-100 duration-300">
                     <img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg" alt="" class="ml-4 w-[50px] aspect-square rounded-full">
-                    <p class='text-xl'>hello</p>
+                    <p class='text-xl max-sm:ml-[-30%]'>hello</p>
                 </li>
             </ul>
         </div>
 
-        <div class="border border-gray-500 lg:w-3/4 md:w-full sm:w-full h-screen max-sm:w-full max-sm:hidden sm:hidden md:hidden lg:block overflow-y-scroll bg-cover bg-scroll bg-bottom bg-no-repeat shadow-lg"  style="background-image:url('https://wallpapershome.com/images/pages/pic_h/24806.jpg');">
+        <div class="h-screen lg:col-span-3 md:col-span-2 sm:col-span-1 max-sm:w-full max-sm:hidden lg:block overflow-y-scroll bg-cover bg-scroll bg-bottom bg-no-repeat shadow-lg"  style="background-image:url('https://wallpapershome.com/images/pages/pic_h/24806.jpg');">
             <div class="w-full aspect-auto grid justify-center items-center mt-[270px]">
                 <p class="p-2 rounded-full bg-cyan-300 text-sm">Select A Chat To Start Messageing</p>
             </div>
